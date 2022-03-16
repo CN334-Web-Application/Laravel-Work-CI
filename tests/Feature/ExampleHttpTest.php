0050428ffd893e5a68cbae9a747de0e24256a589
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-03-16 11:17:59 +0700

    Fix Http test error

--- tests/Feature/ExampleHttpTest.php
+++ tests/Feature/ExampleHttpTest.php
@@ -1,29 +1,19 @@
-<?php
-
-namespace Tests\Feature;
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleHttpTest extends TestCase
+public function testUserCreationEndpointTest()
 {
-    public function testUserCreationEndpointTest()
-    {
-        $name = $this->faker->name();
-        $email = $this->faker->email();
-        $password = "mypassword";
+    $name = $this->faker->name();
+    $email = $this->faker->email();
+    $password = "mypassword";
 
-        $response = $this->postJson('/api/createuser', [
-            'name' => $name, 
-            'email' => $email,
-            'password' => $password,
-            'password_confirmation' => $password
-        ]); 
+    $response = $this->postJson('/api/createuser', [
+        'name' => $name, 
+        'email' => $email,
+        'password' => $password,
+        'password_confirmation' => $password
+    ]); 
 
-        $response
-            ->assertStatus(201)
-            ->assertExactJson([
-                'message' => "Successfully created user!",
-            ]);
-    }
+    $response
+        ->assertStatus(201)
+        ->assertExactJson([
+            'message' => "Successfully created user!",
+        ]);
 }
