cf20d77d784a06f390065c80e56920727fb2f30e
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-03-23 10:26:00 +0700

    Test the TestSeeder that it's work

---
+++ tests/Unit/TestSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Illunminate\Support\Str;
+use Fake\Generator as Faker;
+
+class TestSeeder extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_seeder()
+    {
+        $this->assertTrue(false);
+    }
+}
