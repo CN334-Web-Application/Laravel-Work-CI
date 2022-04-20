f60c89f84b45f78fff31a1a38ba59d606673d289
OHTODO <45880513+OHTODO@users.noreply.github.com>
2022-04-20 13:26:50 +0700

    Increase testsuite

---
+++ tests/BAT/DeleteTaskBAT.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\BAT;
+
+use PHPUnit\Framework\TestCase;
+use Illunminate\Support\Str;
+use Fake\Generator as Faker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Task;
+use App\Models\User;
+
+class DeleteTaskBAT extends TestCase
+{
+   public function something() {
+        $this->assertTrue(true);
+   }
+}
---
+++ tests/BAT/EditTask.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\BAT;
+
+use PHPUnit\Framework\TestCase;
+use Illunminate\Support\Str;
+use Fake\Generator as Faker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Task;
+use App\Models\User;
+
+class EditTaskBAT extends TestCase
+{
+   
+}
