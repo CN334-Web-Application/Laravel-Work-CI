78031aa1887feddf974fb008ba6c94206369e199
real-43 <60128410+real-43@users.noreply.github.com>
2022-04-20 13:40:44 +0700

    login test

--- tests/BAT/LoginBAT.php
+++ tests/BAT/LoginBAT.php
@@ -1,6 +1,17 @@
 <?php
-    class LoginBAT{
-        //clicl login in and go to dashboard
+    namespace Tests\Unit;
+
+    use PHPUnit\Framework\TestCase;
+    use Illunminate\Support\Str;
+    use Fake\Generator as Faker;
+    use Illuminate\Foundation\Testing\RefreshDatabase;
+    use App\Models\Task;
+    use App\Models\User;
+
+    class LoginBAT extends TestCase{
+        //click login in and go to dashboard
+        // public function test_cliclogin()
         //if login input did not have any text wil show error string 
-        
+        //if account does not exist refresh and alert 
+        //if password wrong 
     }
