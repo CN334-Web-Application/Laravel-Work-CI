f903bfdf56b288b4c77a3c3d73315efdd1b4fa03
real-43 <60128410+real-43@users.noreply.github.com>
2022-04-20 15:42:52 +0700

    bat

--- tests/BAT/RegisterBATTest.php
+++ tests/BAT/RegisterBATTest.php
@@ -29,11 +29,11 @@ class RegisterBATTest extends TestCase
          ->click('Regiter')
          ->seePageIs('/register');
     }
-    
-    public function test_new_users_can_register()
+
+    public function test_new_thai_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'name' => 'ธนพล ฟลัวกลา',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
